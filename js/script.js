e19c5d53525d865f7d8621ad08cc9d57f8df3065
Milan Fila <ffilaa1550@gmail.com>
2024-05-07 20:28:35 +0200

    Add JQuery

--- js/script.js
+++ js/script.js
@@ -2,12 +2,40 @@
     $(function(){
        /* Scroll to the section,
        when the user clicks on the link */
-            $('a').click(function(){
-                var section = $(this).attr('href');
-                $('html, body').animate({
-                    scrollTop: $(section).offset().top
-                }, 1000);
-            });
+        $('.jq--scroll-o-mne').click(function(){
+            $('html, body').animate({
+                scrollTop: $(".jq--o-mne").offset().top
+            }, 1000);
+        });
+
+        $('.jq--scroll-info').click(function(){
+            $('html, body').animate({
+                scrollTop: $(".jq--info").offset().top
+            }, 1500);
+        });
+
+        $('.jq--scroll-reference').click(function(){
+            $('html, body').animate({
+                scrollTop: $(".jq--reference").offset().top
+            }, 2000);
+        });
+
+
+        $('.jq--scroll-foto').click(function(){
+            $('html, body').animate({
+                scrollTop: $(".jq--foto").offset().top
+            }, 2500);
+        });
+
+        $('.jq--scroll-zeptejte-se').click(function(){
+            $('html, body').animate({
+                scrollTop: $(".jq--zeptejte-se").offset().top
+            }, 3000);
+        });
+
+
+
+
     });
 })(jQuery);
 
