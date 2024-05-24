cacb1f330558372323738e41674a3ca750986fed
Milan Fila <ffilaa1550@gmail.com>
2024-05-24 16:06:45 +0200

    Add slider

---
+++ JS-training/training.js
@@ -0,0 +1,24 @@
+// Dolar lze napsat pomocí Alt + 36
+// ` lze napsat pomocí Alt + 96
+
+
+let person1 = {
+    name: "John",
+    age: 25,
+    city: "New York"
+}
+
+let person2 = {
+    name: "Jane",
+    age: 22,
+    city: "San Francisco"
+}
+
+let person3 = {
+    name: "Jim",
+    age: 32,
+    city: "Chicago"
+}
+
+console.log(`Jmenuje se ${person1.name}`)
+console.log(`Je mu ${person1.age} let`)
